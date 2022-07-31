7efa81281e0f8c63b4f3c663f65b012223489b4d
Nikolay Ivanov <eoneof@yandex.ru>
2022-07-31 22:28:56 +0300

    feat: add header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,15 @@ function Header() {
     <>
       <header className='header'>
         <div className='header__logo'></div>
+        <div className='header__menu'>
+          <div className='header__burger-icon'></div>
+          <div className='header__email'>
+            <span>email@mail.com</span>
+          </div>
+          <a className='link header__link'>
+            <span>Выйти</span>
+          </a>
+        </div>
       </header>
 
       <div className='divider'></div>
