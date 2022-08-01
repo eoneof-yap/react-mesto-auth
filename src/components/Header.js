0fcc33105a6ce0aae92eb5a43ebce9aeeff0df16
Nikolay Ivanov <eoneof@yandex.ru>
2022-08-01 17:01:20 +0300

    feat: add mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,43 @@
+import { useState } from 'react';
+
 import closeButtonIcon from '../images/closeButton.svg';
 import menuButtonIcon from '../images/burger-menu-icon.svg';
 
 function Header() {
-  const isOpen = true; // TODO get from props
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const hiddenClassName = `${isMenuOpen ? 'header__menu_mobile' : ''}`;
+
+  function handleMenuButtonClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <>
-      <header className='header'>
-        <div className='header__left'>
-          <div className='header__logo'></div>
-          <button
-            className='button header__menu-button'
-            type='button'
-            name='menu-button'
-            title='Меню'
-            // onClick={}>
-            style={{
-              backgroundImage: isOpen
-                ? `url(${closeButtonIcon})`
-                : `url(${menuButtonIcon})`,
-            }}
-          />
+    <header className='header'>
+      <div className='header__actions'>
+        <div className='header__logo'></div>
+        <button
+          className='button header__menu-button'
+          type='button'
+          name='menu-button'
+          title='Меню'
+          onClick={handleMenuButtonClick}
+          style={{
+            backgroundImage: isMenuOpen
+              ? `url(${closeButtonIcon})`
+              : `url(${menuButtonIcon})`,
+          }}
+        />
+      </div>
+      <div className={`header__menu ${hiddenClassName}`}>
+        <div className='divider divider_top'></div>
+        <div className='header__email'>
+          <span>email@mail.com</span>
         </div>
-        <div className='header__right'>
-          <div className='header__menu'>
-            <div className='divider divider_top'></div>
-            <div className='header__email'>
-              <span>email@mail.com</span>
-            </div>
-            <a className='link header__link'>
-              <span>Выйти</span>
-            </a>
-          </div>
-        </div>
-        <div className='divider'></div>
-      </header>
-    </>
+        <a className='link header__link'>Выйти</a>
+      </div>
+      <div className='divider'></div>
+    </header>
   );
 }
 
