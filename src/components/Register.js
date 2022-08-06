fd46075776b041e94b93a43f5d2cee0c3931f669
Nikolay Ivanov <eoneof@yandex.ru>
2022-08-06 19:25:13 +0300

    fix: change register form type for appropriate submit handling

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,8 +12,8 @@ export default function Login(props) {
       <form
         className='form form_place_auth'
         id='register'
-        name='login'
-        action='login'
+        name='register'
+        action='register'
         onSubmit={handleSubmit}>
         <h2 className='form__header form__header_on-dark'>Регистрация</h2>
         <fieldset className='form__fieldset form__fieldset_on-dark'>
@@ -41,7 +41,7 @@ export default function Login(props) {
         <button
           className='button form__submit-button form__submit-button_white'
           type='submit'
-          form='login'>
+          form='register'>
           <span>Зарегистрироваться</span>
         </button>
         <div className='form__link'>
