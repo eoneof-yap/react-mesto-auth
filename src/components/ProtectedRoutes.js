a0d26fe10c2103036d121e11f52eb441ee9614ca
Nikolay Ivanov <eoneof@yandex.ru>
2022-08-11 02:30:33 +0300

    hotfix: disable login screen hide (temporairly)
    i cannot resolve it yet

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -2,18 +2,21 @@ import { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
-import Preloader from './Preloader.js';
+// import Preloader from './Preloader.js';
 
 export const ProtectedRoutes = (props) => {
   const { isLoggedIn } = useContext(CurrentUserContext);
 
-  if (isLoggedIn === null) {
-    // hide login screen on first page load
-    return <Preloader preloaderIsVisible={props.preloaderIsVisible} />;
-  } else if (isLoggedIn === false) {
-    return <Navigate to={props.redirectTo} />;
-  }
+  return isLoggedIn ? <Outlet /> : <Navigate to={props.redirectTo} />;
 
-  return <Outlet />;
-  // Outlet = children
+  // FIXME: hide login screen on page load if logged in !!!
+  // if (isLoggedIn === null) {
+  //   // hide login screen on first page load
+  //   return <Preloader preloaderIsVisible={props.preloaderIsVisible} />;
+  // } else if (isLoggedIn === false) {
+  //   return <Navigate to={props.redirectTo} />;
+  // }
+
+  // return <Outlet />;
+  // // Outlet = children
 };
